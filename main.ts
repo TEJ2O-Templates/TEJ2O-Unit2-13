be9e5ca92db880a01042224f163ba06dc2514683
Patrick Coxall <mr.coxall@mths.ca>
2023-10-30 14:25:24 -0400

    init

--- main.ts
+++ main.ts
@@ -2,7 +2,41 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program showsa while loop
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// press "A" button
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        basic.pause(500)
+        sprite.move(1)
+        loopCounter = loopCounter + 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// press "B" button
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 0)
+    while (loopCounter >= 0) {
+        basic.pause(500)
+        sprite.move(-1)
+        loopCounter = loopCounter - 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
